619eb605cbd803137b0ca4a1391c12c3360e92a2
Burak Mete Erdogan <bugrevealingbme@gmail.com>
2021-12-19 00:27:41 +0300

    ios version added

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Burak Mete Erdoğan on 18.12.2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
